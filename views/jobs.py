aca23e5871b748d6e59c57a3dc850c97fe9ea10e
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2007-10-04 23:07:16 +0000

    Enabled more deletions
    darcs-hash:20071004230716-1d602-1859b5d5f523bc34626723e3ec9509907a9dd67b.gz

--- views/jobs.py
+++ views/jobs.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db import transaction
-from django.http import HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.views.generic import list_detail
